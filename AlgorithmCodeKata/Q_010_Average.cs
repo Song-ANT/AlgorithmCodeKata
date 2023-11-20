929e3a430bb6482b447616c344b0600f7fafbaa0
Song_ANT <songhagyeom@gmail.com>
2023-11-20 10:05:39 +0900

    Update Q_010_Average.cs

--- AlgorithmCodeKata/Q_010_Average.cs
+++ AlgorithmCodeKata/Q_010_Average.cs
@@ -1,4 +1,10 @@
-﻿public class Q_010_Average
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+ */
+
+public class Q_010_Average
 {
     //문제 설명
     //정수 배열 numbers가 매개변수로 주어집니다.numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.

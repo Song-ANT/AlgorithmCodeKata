af4c62329167fbbff80b7be0cf551957fb37519b
Song_ANT <songhagyeom@gmail.com>
2023-11-28 10:23:46 +0900

    Q_040_ThreeWaySystem

--- AlgorithmCodeKata/Program.cs
+++ AlgorithmCodeKata/Program.cs
@@ -10,8 +10,8 @@ namespace AlgorithmCodeKata
     {
         static void Main(string[] args)
         {
-            Q_035_CalculationChange res = new();
-            var answer = res.solution(3, 20, 4);
+            Q_040_ThreeWaySystem res = new();
+            var answer = res.solution(45);
 
             Console.WriteLine(answer);
             //foreach ( var i in answer )
@@ -19,6 +19,63 @@ namespace AlgorithmCodeKata
             //    Console.WriteLine(i);
             //}
         }
+    }
+}
+
+
+public class Q_040_ThreeWaySystem
+{
+    //문제 설명
+    //자연수 n이 매개변수로 주어집니다.n을 3진법 상에서 앞뒤로 뒤집은 후,
+    //이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+    //제한사항
+    //n은 1 이상 100,000,000 이하인 자연수입니다.
+    //입출력 예
+    //n result
+    //45	7
+    //125	229
+    //입출력 예 설명
+    //입출력 예 #1
+
+    //답을 도출하는 과정은 다음과 같습니다.
+    //n (10진법)    n(3진법) 앞뒤 반전(3진법)  10진법으로 표현
+    //45	1200	0021	7
+    //따라서 7을 return 해야 합니다.
+    //입출력 예 #2
+
+    //답을 도출하는 과정은 다음과 같습니다.
+    //n (10진법)    n(3진법) 앞뒤 반전(3진법)  10진법으로 표현
+    //125	11122	22111	229
+    //따라서 229를 return 해야 합니다.
+
+
+    public int solution(int n)
+    {
+        int answer = 0;
+
+        List<int> threeway = new List<int>();
+
+        while (n > 0)
+        {
+            threeway.Add(n % 3);
+            n = (n - n % 3)/3;
+        }
+
+        foreach(var x in threeway) { Console.WriteLine(x); }
+
+        int count = threeway.Count;
+        for (int i = 0; i < count; i++)
+        {
+            int num = 1;
+            for(int j=0; j< count-i; j++)
+            {
+                num *= 3;
+            }
+            answer += threeway[i] * num;
+        }
+
+        return answer;
     }
 }
 
